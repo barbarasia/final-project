82f0ed259d1f037e27fe30e740c4ef40e01a1677
Barbara <bkharkheli@gmail.com>
2024-07-16 11:50:44 +0200

    final version

--- MachineLearning/recommender2.py
+++ MachineLearning/recommender2.py
@@ -12,44 +12,44 @@ import shutil
 class CustomImageDataGenerator(ImageDataGenerator):
     def flow_from_directory(self, directory, *args, **kwargs):
         generator = super().flow_from_directory(directory, *args, **kwargs)
-        self.target_size = kwargs.get('target_size', (224, 224))
+        self.target_size = kwargs.get("target_size", (224, 224))
         self.num_classes = generator.num_classes
         self.filepaths = generator.filepaths
         self.labels = generator.classes
         return generator
 
 # Load the trained model
-model = tf.keras.models.load_model('/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/food_recognition_model4.h5')
+model = tf.keras.models.load_model("/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/food_recognition_model4.h5")
 
 # New class names
 class_names = ['Bread', 'Dairy product', 'Dessert', 'Egg', 'Fried food', 'Meat', 'Noodles-Pasta', 'Rice', 'Seafood', 'Soup', 'Vegetable-Fruit']
 
 # Load wine recommendations from CSV
 try:
-    df_wine_suggestions = pd.read_csv('/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/df_wine_suggestions.csv')
+    df_wine_suggestions = pd.read_csv("/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/df_wine_suggestions.csv")
 except Exception as e:
     st.error(f"Error loading wine suggestions CSV: {e}")
 
 def get_wine_recommendation(food_class):
-    recommendation = df_wine_suggestions[df_wine_suggestions['Food Category'].str.strip() == food_class.strip()]
+    recommendation = df_wine_suggestions[df_wine_suggestions["Food Category"].str.strip() == food_class.strip()]
     if not recommendation.empty:
         wine_info = recommendation.iloc[0]
-        return wine_info['Wine Recommendation'], wine_info['Image URL']
+        return wine_info["Wine Recommendation"], wine_info["Image URL"]
     else:
         return "No recommendation available", ""
 
 # Preprocess the image without augmentation
 def preprocess_image_without_datagen(img_path):
     # Create a temporary directory structure
-    temp_dir = '/tmp/single_image_dir'
-    class_dir = os.path.join(temp_dir, 'class')
+    temp_dir = "/tmp/single_image_dir"
+    class_dir = os.path.join(temp_dir, "class")
     if os.path.exists(temp_dir):
         shutil.rmtree(temp_dir)
     os.makedirs(class_dir)
 
     # Save the image to the class directory
-    temp_img_path = os.path.join(class_dir, 'image.jpg')
-    img = Image.open(img_path).convert('RGB')
+    temp_img_path = os.path.join(class_dir, "image.jpg")
+    img = Image.open(img_path).convert("RGB")
     img = img.resize((224, 224))
     img.save(temp_img_path)
 
@@ -74,7 +74,7 @@ def predict_image_without_datagen(img_path):
     confidence = np.max(predictions)  # Get the confidence of the prediction
     return predicted_class, confidence, get_wine_recommendation(predicted_class), predictions
 
-st.image("/Users/Barbara/Desktop/app_image.png", use_column_width=True)  # Replace with your image path
+st.image("/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/wine_images/app_image.png", use_column_width=True)  # Replace with your image path
 
 st.title("Upload an image of your meal and get a wine recommendation!")
 
@@ -83,7 +83,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     img_path = uploaded_file.name
     img = Image.open(uploaded_file).convert('RGB')  # Ensure image is in RGB format
-    st.image(img, caption='Your Meal Image', use_column_width=True)
+    st.image(img, caption="Your Meal Image", use_column_width=True)
     
     st.write("Classifying...")
     label, confidence, (wine_recommendation, wine_image_url), predictions = predict_image_without_datagen(uploaded_file)
@@ -91,7 +91,7 @@ if uploaded_file is not None:
     st.title(f"It looks like it's {label}")
 
     # Load the metrics from the CSV file
-    metrics_df = pd.read_csv('/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/metrics.csv')
+    metrics_df = pd.read_csv("/Users/Barbara/Desktop/Ironhack/Final_Project/final-project/MachineLearning/metrics.csv")
 
     # Transpose and set the first row as the header
     metrics_df = metrics_df.T
@@ -115,4 +115,4 @@ if uploaded_file is not None:
     st.text(wine_recommendation)
 
     if wine_image_url:
-        st.image(wine_image_url, caption='Recommended
+        st.image(wine_image_url, caption="Recommended Wine", use_column_width=True)
